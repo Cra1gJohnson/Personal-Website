b95fe7ec8be4878b747e6f011139a165d1548adc
cwalke6 <walkercd21@gmail.com>
2025-08-04 21:24:18 -0400

    Added images to the experience timeline

--- src/assets/components/SimpleTimeline.jsx
+++ src/assets/components/SimpleTimeline.jsx
@@ -2,25 +2,33 @@ import React from 'react';
 
 import '../../index.css';
 
+import NGLogo from '../imgs/NG_Logo.jpg';
+import YMCALogo from '../imgs/YMCA_Logo.jpg';
+import UMBCLogo from '../imgs/UMBC_Logo.jpg';
+
 import ShinyText from './TxtComp/ShinyText.jsx';
 
 const SimpleTimeline = () => {
   const events = [
     { date: "2024 May",
       title: "Graduated from UMBC",
-      description: "Graduated with a Bachelors degree in Computer Science. Cum Laude with a GPA of 3.553." 
+      description: "Graduated with a Bachelors degree in Computer Science. Cum Laude with a GPA of 3.553.",
+      img: UMBCLogo
     },
     { date: "2024 June-August",
       title: "Software Engineer Intern at Northrop Grumman",
-      description: "Worked with the SEWIP Program. Worked specifically on DevSecOps. Working with tools like Jenkins. It was challenging since I had never worked with CI/CD tools before. Especially on such a large program. While a daunting task I updgraded the build scripts to work on RHEL8 machines. While also improving efficiency. Reducing build time from 2 hours to 40 minutes."
+      description: "Worked with the SEWIP Program. Worked specifically on DevSecOps. Working with tools like Jenkins. It was challenging since I had never worked with CI/CD tools before. Especially on such a large program. While a daunting task I updgraded the build scripts to work on RHEL8 machines. While also improving efficiency. Reducing build time from 2 hours to 40 minutes.",
+      img: NGLogo
     },
     { date: "2023 June-August",
       title: "Software Engineer Intern at Northrop Grumman",
-      description: "Developed C++ code for a Electronic Warfare System. Wrote scripts in Python, using Regex, to find applicable test data within certain value thresholds. Challenges I overcame during the internship were, learning Git, putting AGILE development process into practice, navigating a in person work environemnt, and having confidence in my solutions."
+      description: "Developed C++ code for a Electronic Warfare System. Wrote scripts in Python, using Regex, to find applicable test data within certain value thresholds. Challenges I overcame during the internship were, learning Git, putting AGILE development process into practice, navigating a in person work environemnt, and having confidence in my solutions.",
+      img: NGLogo
     },
     { date: "2022 June-August",
       title: "YMCA Pool Manager",
-      description: "Being a Pool Manager taugh me lots of soft skills. Having responsibility, communication with leadership, teamwork, and leadership. While at the time I was upset about not getting a internship after my Freshman year, this job was very important to my professional development. Understanding how to professionally deal with patrons, complaints, and communicate."
+      description: "Being a Pool Manager taugh me lots of soft skills. Having responsibility, communication with leadership, teamwork, and leadership. While at the time I was upset about not getting a internship after my Freshman year, this job was very important to my professional development. Understanding how to professionally deal with patrons, complaints, and communicate.",
+      img: YMCALogo
     }
   ];
 
@@ -29,24 +37,36 @@ const SimpleTimeline = () => {
       <div className="timeline-container">
         <h2>
           <ShinyText
-          text="Experience"
-          disabled={false}
-          speed={3}
-          className="timeline-title"
+            text="Experience"
+            disabled={false}
+            speed={3}
+            className="timeline-title"
           />
         </h2>
-        
+
         <div className="timeline-wrapper">
           <div className="timeline-line"></div>
-          
+
           {events.map((event, index) => (
             <div key={index} className="timeline-item">
               <div className="timeline-circle">
                 <div className="timeline-circle-inner"></div>
               </div>
-              
+
               <div className="timeline-content">
-                <div className="timeline-date">{event.date}</div>
+                <div className="timeline-date">
+                  {event.date}
+                  {event.img && (
+                    <img
+                      src={event.img}
+                      className={
+                        event.img === UMBCLogo
+                          ? "timeline-image-umbc" /* UMBC Logo is really large */
+                          : "timeline-image"
+                      }
+                    />
+                  )}
+                </div>
                 <h3 className="timeline-event-title">{event.title}</h3>
                 <p className="timeline-description">{event.description}</p>
               </div>
