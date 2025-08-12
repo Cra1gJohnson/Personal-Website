faf9411003ffa18729883428a408ea53d9240db8
cwalke6 <walkercd21@gmail.com>
2025-08-11 23:19:04 -0400

    Starting framework, notes, and files for blog tab.

--- src/assets/components/Blog.jsx
+++ src/assets/components/Blog.jsx
@@ -6,6 +6,9 @@ function Blog() {
     return (
         <>
             <h1>Blog</h1>
+            <p>These blogs server as a window into my life. Giving my thoughts on current projects I am working on and how I am coming along with them. 
+                As well as once I am done with a project a reflection.
+            </p>
         </>
     )
 }
