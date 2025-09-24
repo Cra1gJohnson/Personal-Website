9a523539e5a4c75af07e878caecef9e741d811b0
Craig Johnson <cjohns65@terpmail.umd.edu>
2025-09-24 19:20:08 -0400

    little changes

--- src/main.jsx
+++ src/main.jsx
@@ -4,20 +4,10 @@ import { createRoot } from 'react-dom/client';
 // client vs 
 import './index.css';
 import App from './App.jsx';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import NotFoundPage from './pages/NotFoundPage.jsx';
-import ResumeRender from './pages/ResumeRender.jsx';
-import ProjectExperience from './pages/ProjectExperience.jsx';
-
-const router = createBrowserRouter([
-  {path: '/', element: <App />},
-  {path: '/resume', element: <ResumeRender />},
-  {path: '/projects', element: <ProjectExperience />},
-  {path: '*', element: <NotFoundPage />}
-]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </StrictMode>,
 );
